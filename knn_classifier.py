2faaddd09d39e0dd9bf084933070e8030c01ea7d
Datascience Course Student <datascience@datascience-amd64.defaultdomain>
2014-11-20 14:50:39 -0800

    added loop for cross validation

--- knn_classifier.py
+++ knn_classifier.py
@@ -5,8 +5,6 @@ from sklearn.cross_validation import train_test_split
 
 DATA_PATH = "/home/datascience/supernovae-classification/" # Make this the /path/to/the/data
 
-
-
 def parse_feature_file(filename):
     df = pd.DataFrame.from_csv(filename)
     return df.reset_index()
@@ -14,8 +12,7 @@ def parse_feature_file(filename):
 # Returns dict of metrics
 def compare_test_results(test, actual):
 	if(len(test) != len(actual)):
-		print "SOMETHING IS WRONG"
-		return
+		raise InputError("input length mismatch")
 	fp_count = 0.0
 	tp_count = 0.0
 	fn_count = 0.0
@@ -37,7 +34,7 @@ def compare_test_results(test, actual):
 		elif test[i] == 0 and actual[i] == 1:
 			fn_count += 1.0
 		else:
-			print "SOMETHING IS WRONG"
+			raise InputError("input contains invalid value")
 
 	return {"tp" : tp_count/total_pos, "fp" : fp_count/total_neg, "tn" : tn_count/total_neg, "fn" : fn_count/total_pos }
 
@@ -50,41 +47,33 @@ def predict_using_probs(probs, threshold):
 			labels.append(0)
 	return labels
 
-
-
+# Parse features and labels
 df = parse_feature_file(DATA_PATH + "samples_features_77811.csv")
 labels = [int(x) for x in df['type'].values]
-
 feat_df = df.drop('type', axis=1)
-# feature_mat = feat_df.values
 feat_mat = feat_df.values
 
-# TODO: keep paired samples together 
-feat_train, feat_test, label_train, label_test = train_test_split(feat_mat, labels, test_size=0.33, random_state=222)
-
-
 neigh = KNeighborsClassifier(n_neighbors=20, weights='distance')
-neigh.fit(feat_train, label_train) 
-test_results = list(neigh.predict(feat_test))
 
-# print "TEST RESULTS:", list(test_results)
-# print "ACTUAL:		", label_test
+n = 5
+## Train and Validate n times
+for i in range(n):
+	# TODO: keep paired samples together 
+	feat_train, feat_test, label_train, label_test = train_test_split(feat_mat, labels, test_size=1.0/n, random_state=i)
+	neigh.fit(feat_train, label_train) 
+	probs = neigh.predict_proba(feat_test)
+	prob_results = predict_using_probs(probs, 0.19)
+	#print probs
+	print compare_test_results(prob_results, label_test)
 
-#print compare_test_results(test_results, label_test)
+# best_thres = 0.0
+# best_total = 0.0
+# for i in range(100):
+# 	thres = i / 100.0
+# 	pr = predict_using_probs(probs, thres)
+# 	result = compare_test_results(pr, label_test)
+# 	if result['tp'] + result['tn'] > best_total:
+# 		best_total = result['tp'] + result['tn']
+# 		best_thres = thres
 
-probs = neigh.predict_proba(feat_test)
-prob_results = predict_using_probs(probs, 0.20)
-#print probs
-print compare_test_results(prob_results, label_test)
-
-best_thres = 0.0
-best_total = 0.0
-for i in range(100):
-	thres = i / 100.0
-	pr = predict_using_probs(probs, thres)
-	result = compare_test_results(pr, label_test)
-	if result['tp'] + result['tn'] > best_total:
-		best_total = result['tp'] + result['tn']
-		best_thres = thres
-
-print "Best thresold is ", best_thres
+# print "Best thresold is ", best_thres
